369dc84c6a496ee5cbc069c69227397b3a3b8214
lahaina <nattolecats@gmail.com>
2025-01-20 18:17:13 +0900

    akita: init

--- aosp_akita.mk
+++ aosp_akita.mk
@@ -24,9 +24,34 @@ $(call inherit-product, device/google/akita/akita_generic.mk)
 
 PRODUCT_NAME := aosp_akita
 PRODUCT_DEVICE := akita
-PRODUCT_MODEL := AOSP on akita
-PRODUCT_BRAND := Android
+PRODUCT_MODEL := Pixel 8a
+PRODUCT_BRAND := google
 PRODUCT_MANUFACTURER := Google
 
 DEVICE_MANIFEST_FILE := \
 	device/google/akita/manifest.xml
+
+# Inherit some common stuff
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# 2by2 Project Flags
+TARGET_IS_PIXEL := true
+CUSTOM_MAINTAINER := lahaina
+
+# Inherit device configuration
+DEVICE_CODENAME := akita
+DEVICE_PATH := device/google/akita
+VENDOR_PATH := vendor/google/akita
+$(call inherit-product, device/google/zuma/lineage_common.mk)
+$(call inherit-product, $(DEVICE_PATH)/$(DEVICE_CODENAME)/device-lineage.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="akita-user 15 AP4A.250105.002 12701944 release-keys" \
+    BuildFingerprint=google/akita/akita:15/AP4A.250105.002/12701944:user/release-keys \
+    DeviceProduct=$(DEVICE_CODENAME)
+
+$(call inherit-product, $(VENDOR_PATH)/$(DEVICE_CODENAME)-vendor.mk)
