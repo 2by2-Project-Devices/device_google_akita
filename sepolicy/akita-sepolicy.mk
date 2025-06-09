1224a80a6aa3668e186c0e0ab5889cdf98fc9c34
kailua <kailua@project2by2.jp>
2025-06-10 07:21:32 +0900

    akita: sepolicy: Address audio denials

--- sepolicy/akita-sepolicy.mk
+++ sepolicy/akita-sepolicy.mk
@@ -4,6 +4,7 @@ BOARD_SEPOLICY_DIRS += device/google/akita/sepolicy/vendor
 BOARD_SEPOLICY_DIRS += device/google/akita/sepolicy/tracking_denials
 BOARD_SEPOLICY_DIRS += device/google/akita/sepolicy/radio
 
+BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/audio/sepolicy/aidl
 BOARD_VENDOR_SEPOLICY_DIRS += device/google/gs-common/gps/pixel/sepolicy
 BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/common
 BOARD_VENDOR_SEPOLICY_DIRS += hardware/google/pixel-sepolicy/vibrator/cs40l26
