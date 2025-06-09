25bebb3eb3b86ca52306bedff27ccfe215be8d10
kailua <kailua@project2by2.jp>
2025-06-10 01:29:50 +0900

    akita: vintf: Add missing IAudioExtension entry

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -6,7 +6,9 @@
 
 TARGET_KERNEL_SOURCE := $(TARGET_KERNEL_DIR)
 
-DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/aam_compatibility_matrix.xml
+DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += \
+    $(DEVICE_PATH)/aam_compatibility_matrix.xml \
+    $(DEVICE_PATH)/compatibility_matrix.xml
 
 # Kernel modules
 BOARD_VENDOR_KERNEL_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist.vendor_kernel_boot
